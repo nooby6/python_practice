2ede06bfd16e7a12b5444adb3bf6959778803771
Audrine Whrite <117525099+nooby6@users.noreply.github.com>
2024-07-19 14:35:16 +0300

    simple calculator

--- main.py
+++ main.py
@@ -5,15 +5,10 @@ from turtle import clear
 root=Tk()
 root.title("Calculator")
 root.geometry("570x600+100+200")
-root.resizable(False,False)
+root.resizable(True,True)
 root.configure(background="black")
 
 equation=""
-
-def press(num):
-    global equation
-    equation=equation+str(num)
-    label_result.config(text=equation)
 
 def show(value):
     global equation
@@ -25,32 +20,46 @@ def clear():
     equation=""
     label_result.config(text=equation)
 
+def calculate():
+    global equation
+    result=""
+    if equation !="":
+        try:
+            result=eval(equation)
+        except:
+            result="Error"
+            equation=""
+    label_result.config(text=result)
+
+
 label_result=Label(root,width=25, height=2, text="0",anchor="e",font=("Arial", 40),bg="black",fg="white")
 label_result.pack()
 
-Button(root,text="C",width=10,height=3, bg="black",fg="white",command=lambda:clear()).pack(x=10,y=100)
-Button(root,text="(",width=10,height=3, bg="black",fg="white",command=lambda:press("(")).pack(x=120,y=100)
-Button(root,text=")",width=10,height=3, bg="black",fg="white",command=lambda:press(")")).pack(x=230,y=100)
-Button(root,text="/",width=10,height=3, bg="black",fg="white",command=lambda:press("/")).pack(x=340,y=100)
+Button(root,text="C",width=10,height=3, bd=2,bg="black",fg="white",command=lambda:clear()).place(x=10,y=100)
+Button(root,text="(",width=10,height=3, bd=1,bg="black",fg="white",command=lambda:show("(")).place(x=130,y=100)
+Button(root,text=")",width=10,height=3, bd=1,bg="black",fg="white",command=lambda:show(")")).place(x=250,y=100)
+Button(root,text="/",width=10,height=3, bd=1,bg="black",fg="white",command=lambda:show("/")).place(x=370,y=100)
 
-Button(root,text="7",width=10,height=3, bg="black",fg="white",command=lambda:press(7)).pack(x=10,y=160)
-Button(root,text="8",width=10,height=3, bg="black",fg="white",command=lambda:press(8)).pack(x=120,y=160)
-Button(root,text="9",width=10,height=3, bg="black",fg="white",command=lambda:press(9)).pack(x=230,y=160)
-Button(root,text="*",width=10,height=3, bg="black",fg="white",command=lambda:press("*")).pack(x=340,y=160)
+Button(root,text="7",width=10,height=3, bd=1,bg="black",fg="white",command=lambda:show("7")).place(x=10,y=180)
+Button(root,text="8",width=10,height=3, bd=1,bg="black",fg="white",command=lambda:show("8")).place(x=130,y=180)
+Button(root,text="9",width=10,height=3, bd=1,bg="black",fg="white",command=lambda:show("9")).place(x=250,y=180)
+Button(root,text="*",width=10,height=3, bd=1,bg="black",fg="white",command=lambda:show("*")).place(x=370,y=180)
 
-Button(root,text="4",width=10,height=3, bg="black",fg="white",command=lambda:press(4)).pack(x=10,y=220)
-Button(root,text="5",width=10,height=3, bg="black",fg="white",command=lambda:press(5)).pack(x=120,y=220)
-Button(root,text="6",width=10,height=3, bg="black",fg="white",command=lambda:press(6)).pack(x=230,y=220)
-Button(root,text="-",width=10,height=3, bg="black",fg="white",command=lambda:press("-")).pack(x=340,y=220)
+Button(root,text="4",width=10,height=3, bd=1,bg="black",fg="white",command=lambda:show("4")).place(x=10,y=260)
+Button(root,text="5",width=10,height=3, bd=1,bg="black",fg="white",command=lambda:show("5")).place(x=130,y=260)
+Button(root,text="6",width=10,height=3, bd=1,bg="black",fg="white",command=lambda:show("6")).place(x=250,y=260)
+Button(root,text="-",width=10,height=3, bd=1,bg="black",fg="white",command=lambda:show("-")).place(x=370,y=260)
 
-Button(root,text="1",width=10,height=3, bg="black",fg="white",command=lambda:press(1)).pack(x=10,y=280)
-Button(root,text="2",width=10,height=3, bg="black",fg="white",command=lambda:press(2)).pack(x=120,y=280)
-Button(root,text="3",width=10,height=3, bg="black",fg="white",command=lambda:press(3)).pack(x=230,y=280)
-Button(root,text="+",width=10,height=3, bg="black",fg="white",command=lambda:press("+")).pack(x=340,y=280)
+Button(root,text="1",width=10,height=3, bd=1,bg="black",fg="white",command=lambda:show("1")).place(x=10,y=340)
+Button(root,text="2",width=10,height=3, bd=1,bg="black",fg="white",command=lambda:show("2")).place(x=130,y=340)
+Button(root,text="3",width=10,height=3, bd=1,bg="black",fg="white",command=lambda:show("3")).place(x=250,y=340)
+Button(root,text="+",width=10,height=3, bd=1,bg="black",fg="white",command=lambda:show("+")).place(x=370,y=340)
 
-Button(root,text="0",width=10,height=3, bg="black",fg="white",command=lambda:press(0)).pack(x=10,y=340)
-Button(root,text=".",width=10,height=3, bg="black",fg="white",command=lambda:press(".")).pack(x=120,y=340)
-Button(root,text="=",width=10,height=3, bg="black",fg="white",command=lambda:press("=")).pack(x=230,y=340)
+Button(root,text="0",width=10,height=3, bd=1,bg="black",fg="white",command=lambda:show("0")).place(x=10,y=420)
+Button(root,text=".",width=10,height=3, bd=1,bg="black",fg="white",command=lambda:show(".")).place(x=130,y=420)
+Button(root,text="^",width=10,height=3, bd=1,bg="black",fg="white",command=lambda:show("^")).place(x=370,y=420)
+Button(root,text="=",width=15,height=5, bd=1,bg="orange",fg="white",command=lambda:calculate()).place(x=250,y=420)
+
 
 
 root.mainloop()
