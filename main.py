68e74f9ccf74e233385b749bf2e9290ba74eda6b
Audrine Whrite <117525099+nooby6@users.noreply.github.com>
2024-07-19 13:52:16 +0300

    simple calculator

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import tkinter
 from tkinter import *
+from turtle import clear
 
 root=Tk()
 root.title("Calculator")
@@ -7,33 +8,26 @@ root.geometry("570x600+100+200")
 root.resizable(False,False)
 root.configure(background="black")
 
+equation=""
 
 label_result=Label(root,width=25, height=2, text="0",anchor="e",font=("Arial", 40),bg="black",fg="white")
 label_result.pack()
 
-Button(root,text="C", width=5, height=1, font=("Arial", 20), bd=1,bg="black",fg="white",borderwidth=0).place(x=0,y=100)
-Button(root,text="+/-", width=5, height=1, font=("Arial", 20), bd=1,bg="black",fg="white",borderwidth=0).place(x=142,y=100)
-Button(root,text="%", width=5, height=1, font=("Arial", 20), bd=1,bg="black",fg="white",borderwidth=0).place(x=284,y=100)
-Button(root,text="/", width=5, height=1, font=("Arial", 20), bd=1,bg="black",fg="white",borderwidth=0).place(x=426,y=100)
+Button(root,text="C",width=11,height=3,font=("Arial", 20),bg="gray",fg="white",command=lambda: clear ()).pack(x=10,y=100)
+Button(root,text="DEL",width=11,height=3,font=("Arial", 20),bg="gray",fg="white",command=lambda: delete()).pack(x=140,y=100)
+Button(root,text="%",width=11,height=3,font=("Arial", 20),bg="gray",fg="white",command=lambda:press("%")).pack(x=270,y=100)
+Button(root,text="/",width=11,height=3,font=("Arial", 20),bg="gray",fg="white",command=lambda:press("/")).pack(x=400,y=100)
 
-Button(root,text="7", width=5, height=1, font=("Arial", 20), bd=1,bg="black",fg="white",borderwidth=0).place(x=0,y=200)
-Button(root,text="8", width=5, height=1, font=("Arial", 20), bd=1,bg="black",fg="white",borderwidth=0).place(x=142,y=200)
-Button(root,text="9", width=5, height=1, font=("Arial", 20), bd=1,bg="black",fg="white",borderwidth=0).place(x=284,y=200)
-Button(root,text="*", width=5, height=1, font=("Arial", 20), bd=1,bg="black",fg="white",borderwidth=0).place(x=426,y=200)
+Button(root,text="7",width=11,height=3,font=("Arial", 20),bg="gray",fg="white",command=lambda:press("7")).pack(x=10,y=180)
+Button(root,text="8",width=11,height=3,font=("Arial", 20),bg="gray",fg="white",command=lambda:press("8")).pack(x=140,y=180)
+Button(root,text="9",width=11,height=3,font=("Arial", 20),bg="gray",fg="white",command=lambda:press("9")).pack(x=270,y=180)
+Button(root,text="*",width=11,height=3,font=("Arial", 20),bg="gray",fg="white",command=lambda:press("*")).pack(x=400,y=180)
 
-Button(root,text="4", width=5, height=1, font=("Arial", 20), bd=1,bg="black",fg="white",borderwidth=0).place(x=0,y=300)
-Button(root,text="5", width=5, height=1, font=("Arial", 20), bd=1,bg="black",fg="white",borderwidth=0).place(x=142,y=300)
-Button(root,text="6", width=5, height=1, font=("Arial", 20), bd=1,bg="black",fg="white",borderwidth=0).place(x=284,y=300)
-Button(root,text="-", width=5, height=1, font=("Arial", 20), bd=1,bg="black",fg="white",borderwidth=0).place(x=426,y=300)
+Button(root,text="4",width=11,height=3,font=("Arial", 20),bg="gray",fg="white",command=lambda:press("4")).pack(x=10,y=260)
+Button(root,text="5",width=11,height=3,font=("Arial", 20),bg="gray",fg="white",command=lambda:press("5")).pack(x=140,y=260)
+Button(root,text="6",width=11,height=3,font=("Arial", 20),bg="gray",fg="white",command=lambda:press("6")).pack(x=270,y=260)
+Button(root,text="-",width=11,height=3,font=("Arial", 20),bg="gray",fg="white",command=lambda:press("-")).pack(x=400,y=260)
 
-Button(root,text="1", width=5, height=1, font=("Arial", 20), bd=1,bg="black",fg="white",borderwidth=0).place(x=0,y=400)
-Button(root,text="2", width=5, height=1, font=("Arial", 20), bd=1,bg="black",fg="white",borderwidth=0).place(x=142,y=400)
-Button(root,text="3", width=5, height=1, font=("Arial", 20), bd=1,bg="black",fg="white",borderwidth=0).place(x=284,y=400)
-Button(root,text="+", width=5, height=1, font=("Arial", 20), bd=1,bg="black",fg="white",borderwidth=0).place(x=426,y=400)
-
-Button(root,text="0", width=11, height=1, font=("Arial", 20), bd=1,bg="black",fg="white",borderwidth=0).place(x=0,y=500)
-Button(root,text=",", width=5, height=1, font=("Arial", 20), bd=1,bg="black",fg="white",borderwidth=0).place(x=284,y=500)
-Button(root,text="=", width=5, height=1, font=("Arial", 20), bd=1,bg="black",fg="white",borderwidth=0).place(x=426,y=500)
 
 
 root.mainloop()
